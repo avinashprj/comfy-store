aa267fe9fc5607102d422c8cee544f66f916fa8e
avipj <avinashprajapati914@gmail.com>
2022-06-10 16:00:32 +0530

    Create toggleSidebar.js

---
+++ src/toggleSidebar.js
@@ -0,0 +1,18 @@
+import { getElement } from './utils.js';
+
+const toggleNav = getElement('.toggle-nav');
+const sidebarOverlay = getElement('.sidebar-overlay');
+const closeBtn = getElement('.sidebar-close');
+
+toggleNav.addEventListener('click', () => {
+  sidebarOverlay.classList.add('show');
+});
+closeBtn.addEventListener('click', () => {
+  sidebarOverlay.classList.remove('show');
+});
+
+window.addEventListener('keyup', (e) => {
+  if (e.key === 'Escape') {
+    sidebarOverlay.classList.remove('show');
+  }
+});
