76d6be1d3699c361a13fcaabb6d10b167c895d5b
avipj <avinashprajapati914@gmail.com>
2022-06-10 16:00:28 +0530

    Create utils.js

---
+++ src/utils.js
@@ -0,0 +1,49 @@
+//   ATTENTION!!!!!!!!!!!
+//   I SWITCHED TO PERMANENT DOMAIN
+//   DATA IS THE SAME JUST A DIFFERENT URL,
+//   DOES NOT AFFECT PROJECT FUNCTIONALITY
+
+const allProductsUrl = 'https://course-api.com/javascript-store-products';
+// temporary single product
+// 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
+const singleProductUrl =
+  'https://course-api.com/javascript-store-single-product';
+
+const getElement = (selection) => {
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(
+    `Please check "${selection}" selector, no such element exist`
+  );
+};
+
+const formatPrice = (price) => {
+  let ConvertedPrice = (price / 100) * 75;
+  const formattedPrice = new Intl.NumberFormat('en-IN', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(ConvertedPrice);
+  return formattedPrice;
+};
+
+const getStorageItem = (item) => {
+  let storateItem = localStorage.getItem(item);
+  if (storateItem) {
+    storateItem = JSON.parse(localStorage.getItem(item));
+  } else {
+    storateItem = [];
+  }
+  return storateItem;
+};
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
+
+export {
+  allProductsUrl,
+  singleProductUrl,
+  getElement,
+  formatPrice,
+  getStorageItem,
+  setStorageItem,
+};
